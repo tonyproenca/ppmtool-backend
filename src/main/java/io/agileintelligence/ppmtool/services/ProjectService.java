74246539c144814390b1ee5b26067ec964379543
António Proença <antpr@BRSP1W8Q9R2.capco.com>
2019-03-05 19:36:04 -0300

    first commit.

---
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,57 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
+    }
+
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(isNull(project)){
+            throw new ProjectIdException("Project ID '"+projectId+"' does not exist");
+
+        }
+
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(isNull(project)){
+            throw new ProjectIdException("Cannot delete project with ID: " + projectId);
+        }
+
+        projectRepository.delete(project);
+    }
+
+}
